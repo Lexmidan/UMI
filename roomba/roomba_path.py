c6fe6203aa681dea765784b2c92f90492455fab5
Alex ASUS win11 <alex.bogdanov@seznam.cz>
2024-09-30 15:56:49 +0200

    Performance improvement

--- roomba/roomba_path.py
+++ roomba/roomba_path.py
@@ -1,32 +1,80 @@
 import numpy as np
 
-def roomba_path(room_size: tuple, starting_point: tuple, litter_list: list, diagonal: bool = False):
+
+class PotentialGrid:
+    """
+    Represents the potential grid of a room with sources.
+    """
+    def __init__(self, room_shape: tuple):
+        self.room_shape = room_shape
+        self.potential_mask = None
+        self.sources = {} #Dict of potential sources with their positions as keys
+        self.allow_diagonal = False
+
+    def initialize_potential_mask(self):
+        """
+        Calculates the potential mask for the given grid shape. The mask is used to
+        calculate the potential grid for the room.
+        """
+
+        self.potential_mask = np.zeros((2*self.room_shape[0], 2*self.room_shape[1]))
+
+        center_x, center_y = self.room_shape[0] - 1, self.room_shape[1] - 1
+        for i in range(2*self.room_shape[0]):
+            for j in range(2*self.room_shape[1]):
+                if self.allow_diagonal:
+                    r = np.sqrt((i - center_x)**2 + (j - center_y)**2)  # L2 distance
+                else:
+                    r = np.abs(i - center_x) + np.abs(j - center_y)  # L1 distance
+                
+                if r == 0:
+                    self.potential_mask[i, j] = 2  # Center point
+                else:
+                    self.potential_mask[i, j] = 2 / (1 + r)  # Decay with 1/r from center
+
+
+    def calculate_potential_in_point(self, point: tuple):
+        """
+        Calculates the potential in the given point by summing the potentials
+        from all sources.
+        """
+        if not self.sources:
+            return 0
+        
+        potential = 0
+        for source_pos in self.sources:
+            if self.sources[source_pos]:
+                mask_x = self.room_shape[0] + point[0] - source_pos[0] - 1
+                mask_y = self.room_shape[1] + point[1] - source_pos[1] - 1
+                potential += self.potential_mask[mask_x, mask_y]
+        return potential
+
+
+def roomba_path(starting_point: tuple,  potential_grid: PotentialGrid = None):
     """
     Finds path for the roomba to clean a room with the given litter list.
     """
-    grid = initialize_potential(room_size, litter_list)
-    initial_grid = grid.copy()
-
     whole_path = [starting_point]
     current_position = starting_point
 
     print("Current position: ", current_position)
-    print("Sources: ", litter_list)
-    while len(litter_list) > 0:
+    print("Sources: ", [source for source in potential_grid.sources])
+
+    # while any source is active
+    while any([potential_grid.sources[source] for source in potential_grid.sources]):
         
-        
-        path = climb_hill(grid, current_position, diagonal)
+        path = climb_hill(current_position, potential_grid)
         print("Climbed to ", path[-1])
         whole_path += path
-
         current_position = path[-1]
-        grid = remove_source_and_update_potential(grid, current_position)
-        litter_list.remove(current_position)
+        print("Removing source at ", current_position)
+        potential_grid.sources[current_position] = False
+
     print('Area clear!')
-    return whole_path, initial_grid
+    return whole_path
 
 
-def climb_hill(potential: np.ndarray, start_pos: tuple, diagonal: bool = False):
+def climb_hill(start_pos: tuple, potential_grid: PotentialGrid = None):
     """
     Climbs the potential hill starting from the given position.
     Maybe I could implement a gradient ascend?
@@ -34,12 +82,12 @@ def climb_hill(potential: np.ndarray, start_pos: tuple, diagonal: bool = False):
 
 
     current_x, current_y = start_pos
-    current_height = potential[current_x, current_y]
+    current_height = potential_grid.calculate_potential_in_point([current_x, current_y])
 
     # Keep track of the path
     path = [(start_pos[0], start_pos[1])]
 
-    if diagonal:
+    if potential_grid.allow_diagonal:
         directions = [(-1, -1), (-1, 0), (-1, 1),
                     (0, -1),           (0, 1),
                     (1, -1),  (1, 0),  (1, 1)]
@@ -51,14 +99,14 @@ def climb_hill(potential: np.ndarray, start_pos: tuple, diagonal: bool = False):
         neighbors = []
         for dx, dy in directions:
             nx, ny = current_x + dx, current_y + dy
-            if 0 <= nx < potential.shape[0] and 0 <= ny < potential.shape[1]:
+            if 0 <= nx < potential_grid.room_shape[0] and 0 <= ny < potential_grid.room_shape[1]:
                 neighbors.append((nx, ny))
         
         # Find the neighbor with the highest height
         max_height = current_height
         next_pos = (current_x, current_y)
         for nx, ny in neighbors:
-            neighbor_height = potential[nx, ny]
+            neighbor_height = potential_grid.calculate_potential_in_point((nx, ny))
             if neighbor_height > max_height:
                 max_height = neighbor_height
                 next_pos = (nx, ny)
@@ -73,27 +121,7 @@ def climb_hill(potential: np.ndarray, start_pos: tuple, diagonal: bool = False):
             break
 
     return path
-
-
-def initialize_potential(grid_shape: tuple, sources: list):
-    """
-    Calculates the potential grid based on the sources
-    """
-
-    potential_grid = np.zeros((grid_shape[0], grid_shape[1]))
-
-    for i in range(grid_shape[0]):
-        for j in range(grid_shape[1]):
-            potential = 0
-            for source in sources:
-                r = L1_distance((i, j), source)
-                if r == 0:
-                    potential += 2  # Source point itself. Value is arbitrary but must be > 1
-                else:
-                    potential += 1 / r  # Additive potential from source
-            potential_grid[i, j] = potential
-    
-    return potential_grid
+        
 
 
 def remove_source_and_update_potential(potential_grid: np.ndarray, removed_source: tuple):
