2f7b9e21ea3d30c5dc6a20421a50716e3f331ee2
Alex ASUS win11 <alex.bogdanov@seznam.cz>
2024-11-07 17:56:57 +0100

    add failed sequence on a level

--- N-Queens/nqueens.py
+++ N-Queens/nqueens.py
@@ -1,16 +1,39 @@
 import numpy as np
 
+# N-queens problem
 n = 8
+
+# Define helper functions
+
+def allDifferent_column(queens):
+    #If the length of the list (N) is equal to number of unique elements in the list, then all elements are unique
+    return len(queens) == len(np.unique(queens))
+
+def allDifferent_diagonal(queens):
+    # Quens are placed diagonally if the difference between their indexes is equal to the difference between their values
+    for i in range(len(queens)):
+        for j in range(i + 1, len(queens)):
+            if abs(i - j) == abs(queens[i] - queens[j]):
+                return False
+    return True
+
+# Another constraint is that there must be N queens on the board
+def nQueens(queens, n):
+    return len(queens) == n
+
+# I can now define a function that checks if all the queens are placed correctly
+def nqueens_satisfied(queens):
+    return allDifferent_column(queens) and allDifferent_diagonal(queens)
 
 # For each row, the possible positions for the queens. Initially, all positions are possible.
 # i corresponds to the row, and the list contains the possible positions for the queens in that row.
 poss_positions = {i: list(range(n)) for i in range(n)}
 
-
-failed_sequences = []
 # The positions of the queens. Initially, no queens are placed.
 # positions[i] is the column of the queen in row i.
 positions = []
+
+failed_sequences = {}
 
 # Keep track of removed positions in each step
 # i-th list doesn't contain dict for i-th row, because in i-th step we don't remove any position from i-th row
@@ -33,7 +56,12 @@ i = 0
 while i < n:
     # Check if there are no possible positions for the queen in row i. Then backtrack
     if len(poss_positions[i]) == 0:
-        failed_sequences.append(positions.copy())
+        
+        if i in failed_sequences:
+            failed_sequences[i].append(positions.copy())
+        else:
+            failed_sequences[i] = [positions.copy()]
+
         backtrack(i)
         i -= 1
         continue
