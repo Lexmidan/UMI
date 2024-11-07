16fbd440a44d6f4e5d99fcfcb440f893ffd656b6
Alex ASUS win11 <alex.bogdanov@seznam.cz>
2024-11-07 16:05:36 +0100

    backtraking

--- N-Queens/nqueens.py
+++ N-Queens/nqueens.py
@@ -1,31 +1,3 @@
-import numpy as np 
-
-# N-queens problem
-n = 8
-
-# Define helper functions
-
-def allDifferent_column(queens):
-    #If the length of the list (N) is equal to number of unique elements in the list, then all elements are unique
-    return len(queens) == len(np.unique(queens))
-
-def allDifferent_diagonal(queens, n):
-    # Quens are placed diagonally if the difference between their indexes is equal to the difference between their values
-    for i in range(n):
-        for j in range(i + 1, n):
-            if abs(i - j) == abs(queens[i] - queens[j]):
-                return False
-    return True
-
-# Another constraint is that there must be N queens on the board
-def nQueens(queens, n):
-    return len(queens) == n
-
-# I can now define a function that checks if all the queens are placed correctly
-def nqueens_satisfied(queens, n):
-    return allDifferent_column(queens) and allDifferent_diagonal(queens, n) and nQueens(queens, n)
-
-
 # For each row, the possible positions for the queens. Initially, all positions are possible.
 # i corresponds to the row, and the list contains the possible positions for the queens in that row.
 poss_positions = {i: list(range(n)) for i in range(n)}
